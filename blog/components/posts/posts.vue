6be9556998935d7896bc55da7f3451ca885c9bd4
Hassane Sow <imthassane@Gmail.com>
2020-05-16 14:57:57 +0200

    Update: load more button color change

--- blog/components/posts/posts.vue
+++ blog/components/posts/posts.vue
@@ -5,7 +5,7 @@
     </div>
     <p class="text-center">
       <button
-        class="mt-10 mb-10 bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        class="mt-10 mb-10 bg-gray-700 text-white font-bold py-2 px-4 rounded-full"
       >Charger plus d'articles</button>
     </p>
   </div>
