e139a4a8242dd24d1882f97370481d60100e02cc
Hassane Sow <imthassane@Gmail.com>
2020-05-19 17:06:50 +0200

    Adding the companiy lawyers

---
+++ barreau/components/cabinets/lawyers.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="m-3 rounded-lg shadow-lg border-b-2 border-red-700">
+      <div>
+        <img v-if="avatar" class="w-64 h-48 object-cover" :src="avatar" :alt="firstName" />
+      </div>
+      <div class="py-3 px-3">
+        <p v-if="firstName && lastName" class="text-lg font-semibold mb-3">{{ firstName }} {{ lastName }}</p>
+        <p v-if="domain && domain.name" class="text-sm font-light">{{ domain.name }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'cabinet-lawyers',
+    props: ['avatar', 'firstName', 'lastName', 'domain']
+  }
+</script>
--- barreau/pages/cabinets/voir/_id.vue
+++ barreau/pages/cabinets/voir/_id.vue
@@ -22,6 +22,21 @@
         </div>
       </div>
     </div>
+    <div class="md:w-5/6 m-auto mt-5">
+      <h1 class="mb-5 text-xl font-semibold pb-3 border-b-2">Avocats du cabinet</h1>
+      <div class="md:flex md:flex-wrap" v-if="cabinet.lawyers">
+        <div
+          v-for="l in cabinet.lawyers" :key="l.id"
+        >
+         <cabinet-lawyers
+          :avatar="l.avatar"
+          :firstName="l.firstName"
+          :lastName="l.lastName"
+          :domain="l.domain"
+         />
+        </div>
+      </div>
+    </div>
   </div>
   <div v-else>
     <!-- TODO: loading component -->
@@ -33,8 +48,9 @@
   import CabinetContact from "../../../components/cabinets/contact";
   import CabinetDomains from "../../../components/cabinets/domains";
   import CabinetPresentation from "../../../components/cabinets/presentation";
+  import CabinetLawyers from "../../../components/cabinets/lawyers";
   export default {
-    components: {CabinetPresentation, CabinetDomains, CabinetContact},
+    components: {CabinetLawyers, CabinetPresentation, CabinetDomains, CabinetContact},
     head: () => ({
       title: "Cabinet Felix groupe"
     }),
@@ -64,23 +80,23 @@
         totalLawyers: 5,
         lawyers: [
           {
-            id: 1, firstName: "Alpha", lastName: "Diallo", avatar: "",
+            id: 1, firstName: "Alpha", lastName: "Diallo", avatar: "https://images.unsplash.com/photo-1537281809317-e1da440af90f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
             domain: {id: 1, name: "Droits de la famille"}
           },
           {
-            id: 1, firstName: "Alpha", lastName: "Diallo", avatar: "",
+            id: 2, firstName: "Tigui", lastName: "Kourouma", avatar: "https://images.unsplash.com/photo-1570406794469-630903944dc1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
             domain: {id: 2, name: "Droits de l'environnement"}
           },
           {
-            id: 1, firstName: "Alpha", lastName: "Diallo", avatar: "",
+            id: 3, firstName: "Mamadou", lastName: "Kaba", avatar: "https://images.unsplash.com/photo-1531483789621-6dc42dfa5078?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
             domain: {id: 3, name: "Droits de l'enfant"}
           },
           {
-            id: 1, firstName: "Alpha", lastName: "Diallo", avatar: "",
+            id: 4, firstName: "Aliou", lastName: "Bah", avatar: "https://images.unsplash.com/photo-1546525848-3ce03ca516f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
             domain: {id: 4, name: "Droits du commerce"}
           },
           {
-            id: 1, firstName: "Alpha", lastName: "Diallo", avatar: "",
+            id: 5, firstName: "Kadiatou", lastName: "Sylla", avatar: "https://images.unsplash.com/photo-1580385537175-37f81495ac1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
             domain: {id: 5, name: "Droits des entreprises"}
           }
         ]
