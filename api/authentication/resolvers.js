1bfa895ce35e99f4b632c398c4b57a468d8c8f99
Hassane Sow <imthassane@Gmail.com>
2020-05-20 20:26:47 +0200

    Adding authentication

---
+++ api/authentication/resolvers.js
@@ -0,0 +1,37 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const authenticate = async (_parent, data, {pool}) => {
+
+    let query = `
+        SELECT
+            cre_id, cre_password
+        FROM t_credentials_cre
+        WHERE cre_email = $1
+    `;
+    let result = await pool.query(query, [data.email]);
+
+    if(!result.rows || !result.rows.length)
+        throw new Error("Cette adresse email n'existe pas");
+
+    const {cre_id, cre_password} = result.rows[0];
+
+    if(!bcrypt.compareSync(data.password, cre_password))
+        throw new Error("Le mot de passe n'est pas correct");
+
+    query = `
+        SELECT
+            permission
+        FROM tj_credentials_permission
+        WHERE cre_id = $1
+    `;
+    result = await pool.query(query, [cre_id])
+    const permissions = result.rows.map(r => r.permission);
+
+    const token = jwt.sign({ id: cre_id }, process.env.JWT_SECRET);
+    return { token, permissions };
+};
+
+module.exports = {
+    Mutation: { authenticate }
+}
--- api/authentication/server.js
+++ api/authentication/server.js
@@ -3,15 +3,28 @@ const { importSchema } = require('graphql-import');
 const { buildFederatedSchema } = require('@apollo/federation');
 
 const GrapqhDatetime = require('graphql-type-datetime');
+const environment = require('dotenv');
 
 const typeDefs = gql(importSchema("./schemas/schema.graphql"));
 
+environment.config();
+
+// Adding the database.
+const pool = require('./database');
+
 const resolvers = {
     DateTime: GrapqhDatetime,
+    ...require('./resolvers')
 };
 
 const schema = buildFederatedSchema({ typeDefs, resolvers });
 
-const server = new ApolloServer({ schema });
+const context = () => {
+    return {
+        pool
+    }
+};
+
+const server = new ApolloServer({ schema, context });
 
 module.exports = server;
