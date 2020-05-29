491b289af12a92ad0d0451b3e72613ff25c4609f
Hassane Sow <imthassane@Gmail.com>
2020-05-29 15:23:26 +0200

    typo fix

--- api/lawyers/index.js
+++ api/lawyers/index.js
@@ -1,5 +1,5 @@
 const server = require('./server');
 
 server.listen({port: 5006}).then(({url}) => {
-    console.log("Lawyersr service running at: " + url);
+    console.log("Lawyers service running at: " + url);
 });
