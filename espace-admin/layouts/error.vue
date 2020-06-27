1726e6d28be78ca9c49825f258cda50866e29b11
Hassane Sow <imthassane@Gmail.com>
2020-06-27 17:38:59 +0200

    Adding error pages

---
+++ espace-admin/layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mt-10 mb-10 md:w-1/2 m-auto">
+
+    <div class="text-center" v-if="error.statusCode === 404">
+      <p class="font-light">Vous vous êtes perdu en chemin, cette page n'existe pas!</p>
+      <not-found-vector />
+    </div>
+
+    <div v-else>
+      <h1>Oups!</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+  import NotFoundVector from "../components/svg/not-found";
+  export default {
+    components: {NotFoundVector},
+    props: ['error'],
+    layout: 'blog' // vous pouvez définir une mise en page personnalisée pour la page d'erreur
+  }
+</script>
