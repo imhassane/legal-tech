06e63eba14752be14a6ebe46ed9472ae99b16a3c
Hassane Sow <imthassane@Gmail.com>
2020-11-05 07:08:48 +0100

    Managing the members who are waiting for validations

---
+++ espace-admin/components/managment/members/waiting-member.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="p-1 mb-2 rounded-lg pb-2 border-b border-gray-200 flex align-middle">
+    <img :src="member.avatar" class="w-16 h-16 object-cover rounded-full mr-2" :alt="member.firstName" />
+    <div class="w-1/6 mr-2">
+      <p class="font-semibold text-sm">
+        {{ member.firstName }} {{ member.lastName.toUpperCase() }}
+      </p>
+      <p class="font-light text-sm">{{ member.type.toLowerCase()}}</p>
+    </div>
+    <div class="w-1/6 mr-2 font-light text-sm">
+      <p class="w-40 text-center mb-1 px-1 bg-green-200 text-green-700 rounded" v-if="member.hasDomains">spécialités définies</p>
+      <p class="w-40 text-center mb-1 px-1 bg-red-200 text-red-700 rounded" v-else>spécialités non définies</p>
+
+      <p class="w-40 text-center mb-1 px-1 bg-green-200 text-green-700 rounded" v-if="member.hasEducation">formations définies</p>
+      <p class="w-40 text-center mb-1 px-1 bg-red-200 text-red-700 rounded" v-else>formations non définies</p>
+
+      <p class="w-40 text-center mb-1 px-1 bg-green-200 text-green-700 rounded" v-if="member.hasCompany">entreprise déclarée</p>
+      <p class="w-40 text-center mb-1 px-1 bg-red-200 text-red-700 rounded" v-else>entreprise non déclarée</p>
+    </div>
+    <div class="w-2/5 py-3">
+      <button class="bg-red-700 text-white font-semibold h-8 w-24 rounded mr-1">Valider</button>
+      <button class="border-red-700 border bg-white text-red-700 font-semibold h-8 w-24 rounded">Rejeter</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "waiting-member",
+    props: ["member"]
+  }
+</script>
--- espace-admin/pages/managment/members/waiting.vue
+++ espace-admin/pages/managment/members/waiting.vue
@@ -1,81 +1,54 @@
 <template>
-  <div>
+  <div v-if="$apollo.loading"></div>
+  <div v-else>
     <page-title title="Membres en attente" />
     <p class="my-3 py-3 px-4 border-l-4 border-gray-800 bg-gray-300">
       Vous pourrez à tout moment changer le statut d'un membre
     </p>
 
-    <div class="my-12">
-      <table class="w-full">
-        <thead>
-          <th>Photo</th>
-          <th>Prénom & Nom</th>
-          <th>Date d'inscription</th>
-          <th>Type</th>
-          <th>Plus de details</th>
-          <th>Valider</th>
-          <th>Rejeter</th>
-        </thead>
-        <tbody>
-          <tr v-for="m in members" :key="m.id" class="py-4 my-4 border-b-2 border-gray-300">
-            <td>
-              <img :src="m.avatar" class="w-12 h-12 object-cover" :alt="m.firstName" />
-            </td>
-            <td>{{ m.firstName }} {{ m.lastName }}</td>
-            <td>{{ m.createdAt }}</td>
-            <td>{{ m.type }}</td>
-            <td></td>
-            <td><button class="py-2 px-4 bg-blue-400">Valider</button></td>
-            <td><button>Rejeter</button></td>
-          </tr>
-        </tbody>
-      </table>
+    <div class="my-8">
+      <input
+        type="text" placeholder="Cherchez un membre"
+        v-model="search" class="w-full p-1 border-b-2 border-gray-200 font-light"
+      />
+    </div>
+
+    <div class="my-12" v-if="members">
+      <errors-box type="info" message="Aucun utilisateur en attente de validation" v-if="members.length === 0" />
+      <waiting-member v-for="m in members" :key="m.id" :member="m" v-else
+                      v-if="(m.firstName + ' ' + m.lastName).toLowerCase().includes(search.toLowerCase())" />
     </div>
   </div>
 </template>
 
 <script>
+  import gql from "graphql-tag";
   import PageTitle from "../../../components/page-title";
+  import WaitingMember from "../../../components/managment/members/waiting-member";
+  import ErrorsBox from "../../../components/errors";
+
+  const MEMBERS_QUERY = gql`
+    query ($start: Int!, $limit: Int!){
+        members(type: ALL, status: PENDING, start: $start, limit: $limit) {
+            id, firstName, lastName, avatar, type,
+            hasDomains, hasEducation, hasCompany
+        }
+    }
+   `;
+
   export default {
-    components: {PageTitle},
+    components: {ErrorsBox, WaitingMember, PageTitle},
     head: () => ({
       title: "Membres en attente"
     }),
-    computed: {
-      members: () => ([
-        {
-          id: 1,
-          firstName: "Alpha Mamadou",
-          lastName: "Barry",
-          avatar: "https://images.unsplash.com/photo-1579192181049-2aa87e49df2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-          createdAt: "22 Juin 2020",
-          type: "LAWYER"
-        },
-        {
-          id: 2,
-          firstName: "Alpha Mamadou",
-          lastName: "Barry",
-          avatar: "https://images.unsplash.com/photo-1579192181049-2aa87e49df2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-          createdAt: "22 Juin 2020",
-          type: "LAWYER"
-        },
-        {
-          id: 3,
-          firstName: "Jean",
-          lastName: "Koivoguy",
-          avatar: "https://images.unsplash.com/photo-1538384690625-e2e7bcf9e402?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-          createdAt: "22 Juin 2020",
-          type: "LAWYER"
-        },
-        {
-          id: 4,
-          firstName: "Fodé",
-          lastName: "Soumah",
-          avatar: "https://images.unsplash.com/photo-1563721572772-fbf713fff374?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-          createdAt: "22 Juin 2020",
-          type: "LAWYER"
-        },
-      ])
+    data: () => ({ start: 0, limit: 20, search: "" }),
+    apollo: {
+      members: {
+        query: MEMBERS_QUERY,
+        variables: function() {
+          return { start: this.start, limit: this.limit }
+        }
+      }
     }
   }
 </script>
